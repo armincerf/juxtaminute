4f0bdd66fb5d79cf2421e2e7bf89a5fb62a6184b
Alex Davis <alx@juxt.pro>
2020-08-30 22:18:02 +0100

    Add graphql subscriptions example

--- src/main/juxtaminute/app.cljs
+++ src/main/juxtaminute/app.cljs
@@ -1,4 +1,56 @@
-(ns juxtaminute.app)
+(ns juxtaminute.app
+  (:require [re-graph.core :as re-graph]
+            [reagent.core :as r]
+            [re-frame.core :as rf]
+            [reagent.dom :as rdom]))
 
-(defn init []
-  (prn "init"))
+(def questions-query
+  (str
+   "{"
+   "questions(order_by: {askedTime: desc}) {
+    user
+    question
+    id
+    askedTime
+    answer
+  }"
+   "}"))
+
+(re-graph/init {:ws {:url "wss://cute-ape-95.hasura.app/v1/graphql"}
+                :http {:url "https://cute-ape-95.hasura.app/v1/graphql"}})
+
+(rf/reg-event-db
+ ::success
+ (fn [db [_ k res]]
+   (assoc-in db [:data k] (:data res))))
+
+(rf/reg-sub
+ ::data
+ (fn [db [_ k]]
+   (get-in db [:data k])))
+
+(defn root-view
+  []
+  (let [data @(rf/subscribe [::data :questions])]
+    [:<>
+     [:button {:on-click #(rf/dispatch [::re-graph/subscribe
+                                        :questions
+                                        questions-query
+                                        {}
+                                        [::success :questions]])}
+      "get questions"]
+     [:div.questions
+      (for [q (:questions data)]
+        ^{:key q}
+        [:div.question
+         [:h3 (:question q)]
+         [:p (:answer q)]])]]))
+
+
+(defn ^:export ^:dev/after-load render []
+  (rdom/render [root-view]
+                  (js/document.getElementById "root")))
+
+(defn ^:export init []
+  (rf/dispatch-sync [:initialize])
+  (render))
