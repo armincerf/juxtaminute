122a7422547545732efcf30abbe1ee51c72b038a
Alex Davis <alx@juxt.pro>
2021-08-12 21:00:29 +0100

    wip

--- src/main/juxtaminute/app.cljs
+++ src/main/juxtaminute/app.cljs
@@ -1,38 +1,34 @@
 (ns juxtaminute.app
   (:require [re-graph.core :as re-graph]
+            [tick.alpha.api :as tick]
             [reagent.core :as r]
             [re-frame.core :as rf]
             [medley.core :as medley]
             [reagent.dom :as rdom]
             [juxtaminute.common :as common]
             [clojure.string :as str]))
+(js/console.log "start")
+(when-let [stuff (some-> js/document
+                         (.getElementById "userInfo")
+                         (.getAttribute "content"))]
+  (do (def me (js->clj (.parse js/JSON stuff)
+                       :keywordize-keys true))
+      (def my-name (or  (:nickname me) (:displayName me)))))
 
-(def me {:id  "1"
-         :name "test1"})
-
-(def regraph-config {:ws {:url "wss://cute-ape-95.hasura.app/v1/graphql"}
+(def regraph-config {:ws {:url "wss://cute-ape-95.hasura.app/v1/graphql"
+                          :supported-operations #{:subscribe}}
                      :http {:url "https://cute-ape-95.hasura.app/v1/graphql"}})
-
-(def questions-query
-  (str
-   "{"
-   "questions(order_by: {askedTime: desc}) {
-    user
-    question
-    id
-    askedTime
-    answer
-  }"
-   "}"))
 
 (def game-users-query
   "{
   game_users {
     user {
       name
+      total_score
       votes {
         voter {
           name
+          id
         }
       }
       id
@@ -42,12 +38,20 @@
   }
 }")
 
+(def users-query
+  "{
+  users {
+    id
+  }
+}")
+
 (def topics-query
   "{
   game_topics {
     name
 votes {
         voter {
+          id
           name
         }
       }
@@ -59,20 +63,38 @@ votes {
   game_round(limit: 1, order_by: {created: desc}) {
     chosen_user
     ends_at
+    ended
     topic
     id
-    challenger
     challenge
     created
+    userByWinner {
+      name
+    }
+    challengerUser {
+      name
+      id
+      game_users {
+        score
+      }
+    }
   }
 }")
+
+(defn button
+  [opts text]
+  [:button
+   (merge
+    {:disabled @(rf/subscribe [::loading?])}
+    opts)
+   text])
 
 (defn all-votes
   [k vals]
   (->> vals
        (map (if k (comp :votes k) :votes))
        (apply concat)
-       (map (comp :name :voter))))
+       (map (comp :id :voter))))
 
 (defn chosen-user
   [users]
@@ -83,13 +105,15 @@ votes {
   (let [users @(rf/subscribe [::users])
         voted-user @(rf/subscribe [::voted-user])
         round @(rf/subscribe [::round])]
-    (cond
-      (some? round)
-      :round
-      (some? voted-user)
-      :topic-voting
-      :else
-      :waiting-room)))
+    (if (some #(= (:id me) (get-in % [:user :id])) users)
+      (cond
+        (= false (:ended round))
+        :round
+        (some? voted-user)
+        :topic-voting
+        :else
+        :waiting-room)
+      :join-game)))
 
 
 (rf/reg-event-db
@@ -148,11 +172,109 @@ votes {
  }
  }")
 
+(def start-round-mutation
+  "mutation start_round($id: Int!, $ends_at: timestamptz) {
+  update_game_round_by_pk(pk_columns: {id: $id}, _set: {ends_at: $ends_at}) {
+    id
+  }
+}
+")
+
+(def round-won-mutation
+  "mutation round_won($id: Int!, $winner: String) {
+  update_game_round_by_pk(pk_columns: {id: $id}, _set: {winner: $winner}) {
+    id
+  }
+}
+")
+
+(def reset-vote-mutation
+  "mutation reset_votes {
+  delete_vote(where: {}) {
+    affected_rows
+  }
+}")
+
+(def end-round-mutation
+  "mutation end_round($id: Int!, $user_id: String!) {
+  update_game_round_by_pk(pk_columns: {id: $id}, _set: {ended: true}) {
+    id
+  }
+  delete_vote(where: {}) {
+    affected_rows
+  }
+  update_game_users_by_pk(pk_columns: {user_id: $user_id}, _set: {chosen: false}) {
+    user_id
+  }
+  update_game_users(where: {score: {_neq: 0}}, _set: {score: 0}) {
+    affected_rows
+  }
+}")
+
+(def quit-game-mutation
+  "mutation quit_game($user_id: String!) {
+  delete_game_users_by_pk(user_id: $user_id) {
+    user_id
+  }
+}
+")
+
+(def join-new-game-mutation
+  "mutation join_game($user_id: String!, $id: String!, $name: String) {
+  insert_users(objects: {id: $id, name: $name}) {
+    affected_rows
+  }
+  insert_game_users_one(object: {user_id: $user_id}) {
+    user_id
+  }
+}
+")
+
+(def join-game-mutation
+  "mutation join_game($user_id: String!) {
+  insert_game_users_one(object: {user_id: $user_id}) {
+    user_id
+  }
+}
+")
+
+(def challenge-mutation
+  "mutation challenge($id: Int!, $challenge: String, $challenger: String) {
+  update_game_round_by_pk(pk_columns: {id: $id}, _set: {challenge: $challenge, challenger: $challenger}) {
+    id
+  }
+}
+")
+
+(def challenge-agree-mutation
+  "mutation challenge_agree($user_id: String!, $score: Int, $id: Int!, $ends_at: timestamptz, $_eq: String) {
+  update_game_users(where: {user_id: {_eq: $_eq}}, _set: {chosen: false}) {
+    affected_rows
+  }
+  update_game_users_by_pk(pk_columns: {user_id: $user_id}, _set: {chosen: true, score: $score}, _inc: {}) {
+    user_id
+  }
+  update_game_round_by_pk(pk_columns: {id: $id}, _set: {ends_at: $ends_at, challenger: null, challenge: null, chosen_user: $user_id}) {
+    id
+  }
+}")
+
+(def challenge-disagree-mutation
+  "mutation challenge_disagree($user_id: String!, $score: Int, $id: Int!, $ends_at: timestamptz) {
+  update_game_users_by_pk(pk_columns: {user_id: $user_id}, _set: {score: $score}) {
+    user_id
+  }
+  update_game_round_by_pk(pk_columns: {id: $id}, _set: {ends_at: $ends_at, challenger: null, challenge: null}) {
+    id
+  }
+}")
+
 (rf/reg-event-fx
  ::vote-for-topic
  (fn [{:keys [db]} [_ topic]]
-   (let [deciding-vote? (= 1 (count (:votes topic)))
+   (let [deciding-vote? (<= 1 (count (:votes topic)))
          chosen-user (and deciding-vote? (chosen-user (get-in db [:data :game-users :game_users])))]
+     (prn (:votes topic) "hi")
      {:fx [[:dispatch [::re-graph/mutate
                        :vote-for-topic
                        vote-for-topic-mutation
@@ -171,8 +293,8 @@ votes {
 (rf/reg-event-fx
  ::vote-for-user
  (fn [_ [_ user]]
-   (js/console.log user)
-   (let [deciding-vote? (= 1 (count (:votes user)))]
+   (prn user "votes" (count (:votes user)))
+   (let [deciding-vote? (<= 1 (count (:votes user)))]
      {:fx [[:dispatch [::re-graph/mutate
                        :vote-for-user
                        vote-for-user-mutation
@@ -185,6 +307,16 @@ votes {
                          choose-user-mutation
                          {:user_id (:id user)}
                          []]])]})))
+
+(rf/reg-sub
+ ::db
+ (fn [db]
+   db))
+
+(rf/reg-sub
+ ::loading?
+ (fn [db]
+   (seq (get-in db [:re-graph :re-graph.internals/default :http :requests]))))
 
 (rf/reg-sub
  ::data
@@ -218,7 +350,7 @@ votes {
 (defn topic-vote
   []
   (let [topics @(rf/subscribe [::topics])
-        voted? (some #{(:name me)} (all-votes nil topics))]
+        voted? (some #{(:id me)} (all-votes nil topics))]
     [:div.topics
      [common/pprint-code topics]
      (for [{:keys [name] :as topic} topics
@@ -230,64 +362,236 @@ votes {
         (when (seq voter-names)
           [:p (str "Votes so far - " (str/join ", " voter-names))])
         (when-not voted?
-          [:button
+          [button
            {:on-click #(rf/dispatch [::vote-for-topic topic])}
            "Vote"])])]))
 
 (defn waiting-room
   []
   (let [users @(rf/subscribe [::users])
-        voted? (some #{(:name me)} (all-votes :user users))]
+        voted? (some #{(:id me)} (all-votes :user users))]
     [:div.users
      [common/pprint-code users]
+     (if voted?
+       [:p "Thanks for voting, waiting for others..."]
+       [:p "Please vote for who you want to speak"])
      (for [{:keys [user]} users
            :let [votes (:votes user)
-                 voter-names (map #(get-in % [:voter :name]) votes)]]
+                 voter-names (map #(get-in % [:voter :name]) votes)
+                 me? (= (:id me) (:id user))]]
        ^{:key user}
        [:div.user
-        [:h3 (str "User - " (:name user))]
+        [:h3 (str "User - " (:name user) (when me? " (you)"))]
         (when (seq voter-names)
           [:p (str "Votes so far - " (str/join ", " voter-names))])
-        (when-not voted?
-          [:button
+        (when-not (or voted? me?)
+          [button
            {:on-click #(rf/dispatch [::vote-for-user user])}
            "Vote"])])]))
 
+(defn countdown
+  [round]
+  (r/with-let [updater (r/atom 130)
+               timer-fn (js/setInterval #(swap! updater dec) 500)]
+    (let [end-time (tick/instant (tick/parse (:ends_at round)))
+          duration (tick/duration
+                    {:tick/beginning (tick/instant)
+                     :tick/end end-time})
+          hours (tick/hours duration)
+          minutes (tick/minutes (tick/- duration
+                                        (tick/new-duration hours :hours)))
+          seconds (tick/seconds (tick/- duration
+                                        (tick/new-duration minutes :minutes)
+                                        (tick/new-duration hours :hours)))
+          ;;stupid but not sure if theres a better way
+          _ @updater]
+      (cond
+        (>= minutes 1)
+        [:p.countdown (str "Get Ready! Starting in " seconds " seconds")]
+        (tick/< (tick/instant) end-time)
+        [:<>
+         [:p "Topic is " (:topic round)]
+         [:p.timer (str seconds " seconds remaining! (reduced timer for test)")]
+         (if (= (:id me) (:chosen_user round))
+           [:p "SPEAK!!"]
+           (for [challenge ["Hesitation" "Deviation" "Repetition"]]
+             ^{:key challenge}
+             [button
+              {:on-click #(rf/dispatch [::re-graph/mutate
+                                        :challenge
+                                        challenge-mutation
+                                        {:id (:id round)
+                                         :challenge challenge
+                                         :challenger (:id me)}
+                                        [:challenge-success]])}
+              (str challenge "!")]))]
+        :else
+        (if (nil? (:userByWinner round))
+          (rf/dispatch [::re-graph/mutate
+                        :won-round
+                        round-won-mutation
+                        {:id (:id round)
+                         :winner (:chosen_user round)}
+                        [:won-round-success]]))
+        (js/clearInterval timer-fn)
+        "Time's up!"))
+    (finally (js/clearInterval timer-fn))))
+
 (defn timer-view
   [round]
-  [:div "timer"])
+  [:<>
+   [countdown round]])
 
-(defn challenge
+(defn challenge-view
   [round]
-  [:div "Challenge!"])
+  (let [user (:challengerUser round)
+        end-time (tick/instant (tick/parse (:ends_at round)))
+        duration (tick/duration
+                  {:tick/beginning (tick/instant)
+                   :tick/end end-time})
+        seconds-left (tick/seconds duration)
+        voted-user-id (:id @(rf/subscribe [::voted-user]))]
+    [:div
+     [:h2 "Challenge!"]
+     [:p (str (:name user) " has submitted the challange - " (:challenge round))]
+     (when-not (or (= (:id me)
+                      (:id user))
+                   (= (:id me)
+                      voted-user-id))
+       [:div.buttons
+        [button
+         {:on-click #(rf/dispatch [::re-graph/mutate
+                                   :challenge-agree
+                                   challenge-agree-mutation
+                                   {:score (inc (:score (first (:game_users user))))
+                                    :user_id (:id user)
+                                    :id (:id round)
+                                    :_eq voted-user-id
+                                    :ends_at (common/add (+ 2 seconds-left)
+                                                         {:duration :seconds})}
+                                   [:challenge-agree-success]])}
+         "Agree"]
+        [button
+         {:on-click #(rf/dispatch [::re-graph/mutate
+                                   :challenge-disagree
+                                   challenge-disagree-mutation
+                                   {:score (dec (:score (first (:game_users user))))
+                                    :user_id (:id user)
+                                    :id (:id round)
+                                    :ends_at (common/add (+ 2 seconds-left)
+                                                         {:duration :seconds})}
+                                   [:challenge-disagree-success]])}
+         "Disagree"]])]))
+
+
+
+(defn round-over
+  [round]
+  (let [users (:game_users @(rf/subscribe [::data :game-users]))]
+    [:div
+     [:div "Round over! The winner was "
+      (get-in round [:userByWinner :name])]
+     [:div.scores
+      "scores: "
+      (str/replace (str/join ", " (map (juxt (comp :name :user) :score) users))
+                   #"\[|\]|" "")]
+     [button
+      {:on-click #(rf/dispatch [::re-graph/mutate
+                                :new-round
+                                end-round-mutation
+                                {:id (:id round)
+                                 :user_id (or (:id @(rf/subscribe [::voted-user]))
+                                              (:id me))}
+                                [:new-round-success]])}
+      "New Round"]
+     [button
+      {:on-click #(do
+                    (prn "quit")
+                    (rf/dispatch [::re-graph/mutate
+                                  :quit
+                                  quit-game-mutation
+                                  {:user_id (:id me)}
+                                  [:quit-success]]))}
+      "Quit game"]]))
 
 (defn round-view
   []
-  (let [{:keys [topic ends_at challenge challenger] :as round}
+  (let [{:keys [topic userByWinner ends_at challenge] :as round}
         @(rf/subscribe [::round])
         user @(rf/subscribe [::voted-user])]
     [:<>
      [common/pprint-code round]
      (cond
+       (some? userByWinner)
+       [round-over round]
        (some? challenge)
-       [challenge round]
+       [challenge-view round]
        (some? ends_at)
        [timer-view round]
        :else
        [:div
-        [:h3 (str (:name user) ", are you ready to start?")]
         (if (= (:id user) (:id me))
-          [:button "Lets go!"]
+          [:<>
+           [:h3 (str (:name user) ", are you ready to start?")]
+           [button
+
+            {:on-click #(rf/dispatch [::re-graph/mutate
+                                      :start-round
+                                      start-round-mutation
+                                      {:id (:id round)
+                                       :ends_at (common/add 65 {:duration :seconds})}
+                                      []])}
+            "Lets go!"]]
           [:p "waiting for " (:name user)])])]))
+
+(rf/reg-event-fx
+ :generic-success
+ (fn [_ _]
+   (prn "success")))
+
+(rf/reg-event-fx
+ ::join-game
+ (fn [_ [_ resp]]
+   (prn resp)
+   (let [users (get-in resp [:data :users])
+         exists? (some #(= (:id me) (:id %)) users)]
+     (prn exists?)
+     {:fx [[:dispatch [::re-graph/mutate
+                       :join-game
+                       (if exists?
+                         join-game-mutation
+                         join-new-game-mutation)
+                       (if exists?
+                         {:user_id (:id me)}
+                         {:user_id (:id me)
+                          :id (:id me)
+                          :name my-name})
+                       [:generic-success]]]]})))
+
+(defn join-game
+  []
+  [:div
+   [:h3 "Not in the game"]
+   [button
+    {:on-click #(rf/dispatch [::re-graph/query
+                              users-query
+                              {}
+                              [::join-game]])}
+    "Join now"]])
 
 (defn root-view
   []
-  (let [state (current-state)]
-    [common/pprint-code (str "Current state = " state)]
-    (case state
-      :round [round-view]
-      :topic-voting [topic-vote]
-      :waiting-room [waiting-room])))
+  (if my-name
+    (let [state (current-state)]
+      [:<>
+                                        ;[common/pprint-code @(rf/subscribe [::db])]
+       [:p "you are logged in as " my-name]
+       (case state
+         :join-game [join-game]
+         :round [round-view]
+         :topic-voting [topic-vote]
+         :waiting-room [waiting-room])])
+    [:p "loading..."]))
 
 
 (defn ^:export ^:dev/after-load render []
--- src/main/juxtaminute/common.cljs
+++ src/main/juxtaminute/common.cljs
@@ -1,5 +1,8 @@
 (ns juxtaminute.common
   (:require
+   [tick.alpha.api :as tick]
+   [reagent.core :as r]
+   [re-frame.core :as rf]
    [clojure.pprint :as pprint]))
 
 (defn pprint-str
@@ -8,7 +11,27 @@
 
 (defn pprint-code
   [x]
-  [:code
-   {:style {:text-align "left"}}
-   [:pre (pprint-str x)]])
+  (when false
+    [:code
+     {:style {:text-align "left"}}
+     [:pre (pprint-str x)]]))
 
+(defn add
+  "Add duration unit (defaults to minutes) to an instant (now, by default).
+  Duration units available are :millis :seconds :minutes :hours :days Returns
+  `java.util.Date.`"
+  ([minutes] (add minutes {:now (tick/instant) :duration :minutes}))
+  ([amount {:keys [now duration] :or {duration :minutes}}]
+   (->> duration
+        (tick/new-duration amount)
+        (tick/+ (or now (tick/instant))))))
+
+(defn subtract
+  "Subtract duration unit (defaults to minutes) from an instant (now, by default).
+  Duration units available are :millis :seconds :minutes :hours :days Returns
+  `java.util.Date.`"
+  ([minutes] (subtract minutes {:now (tick/instant) :duration :minutes}))
+  ([amount {:keys [now duration] :or {duration :minutes}}]
+   (->> duration
+        (tick/new-duration amount)
+        (tick/- (or now (tick/instant))))))
